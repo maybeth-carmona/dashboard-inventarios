47411940d6a15974f1c7cfe76e70c3f686001082
maybeth-carmona <72000065+maybeth-carmona@users.noreply.github.com>
2026-04-10 11:36:00 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+from datetime import datetime
+
 # =====================================================
 # 🚚 SEGUIMIENTO A PROVEEDORES (CORRECTO)
 # =====================================================
